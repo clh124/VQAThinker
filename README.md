d7eb800f195e4af814ea09734215647326dfb1f6
clh124 <caolinhan@sjtu.edu.cn>
2025-08-04 08:14:34 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# VQAThinker: Exploring Generalizable and Explainable Video Quality Assessment via Reinforcement Learning
+
+
+
+## 🛠️ Setup
+
+```bash
+conda create -n vqathinker python=3.10
+conda activate vqathinker
+bash setup.sh
+```
+
+## 💪🏻 Training
+
+``bash run_scripts/run_grpo_lsvq.sh``
+
+
+### For your own data
+
+The jsonl has the format as follows:
+
+```json
+{
+  "id": 0, 
+  "dataset_name": "LSVQ", 
+  "image": ["yfcc-batch16/13078.mp4"], 
+  "conversations": [
+    {"from": "human", "value": "You are doing the video quality assessment task. Here is the question: What is your overall rating on the quality of this video? The rating should be a float between 1 and 5, rounded to two decimal places, with 1 representing very poor quality and 5 representing excellent quality."}, 
+    {"from": "gpt", "value": 3.418571}
+    ]
+}
+```
+<!-- 
+## 🤝 Acknowledgements
+
+We would like to express our sincere gratitude to [DeepSeek](https://github.com/deepseek-ai/DeepSeek-R1), [Open-R1](https://github.com/huggingface/open-r1), [QwenVL](https://github.com/QwenLM/Qwen2.5-VL), [Open-R1-Multimodal](https://github.com/EvolvingLMMs-Lab/open-r1-multimodal), [R1-V](https://github.com/Deep-Agent/R1-V), [RefCOCO](https://github.com/lichengunc/refer), [RefGTA](https://github.com/mikittt/easy-to-understand-REG/tree/master/pyutils/refer2), [LLaMA-Factory](https://github.com/hiyouga/LLaMA-Factory), [OVDEval](https://github.com/om-ai-lab/OVDEval), [GUI-Testing-Arena](https://huggingface.co/datasets/songjah/GTArena-UI-Defects), and [LISA](https://github.com/dvlab-research/LISA) for providing open-source resources that contributed to the development of this project.
+
+## ⭐️ Citation
+
+If you find this project useful, welcome to cite us.
+
+```bib
+@article{shen2025vlm,
+  title={Vlm-r1: A stable and generalizable r1-style large vision-language model},
+  author={Shen, Haozhan and Liu, Peng and Li, Jingcheng and Fang, Chunxin and Ma, Yibo and Liao, Jiajia and Shen, Qiaoli and Zhang, Zilun and Zhao, Kangjia and Zhang, Qianqian and Xu, Ruochen and Zhao, Tiancheng },
+  journal={arXiv preprint arXiv:2504.07615},
+  year={2025}
+}
+``` -->
---
+++ run_scripts/run_grpo_internvl.sh
@@ -0,0 +1,59 @@
+PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+export REPO_HOME="${PROJECT_ROOT}"
+echo "REPO_HOME: $REPO_HOME"
+# on remote
+data_paths="/tos-bjml-researcheval/wenfarong/caolinhan/data/LSVQ/labels_train.jsonl" 
+image_folders="/tos-bjml-researcheval/wenfarong/caolinhan/data/LSVQ/"
+model_path="/dev/shm/InternVL3-8B-motion/"
+is_reward_customized_from_vlm_module=False
+echo "data_paths: $data_paths"
+echo "image_folders: $image_folders"
+
+export EXP_NAME="InternVL3-8B_GRPO" # TODO: change this to your own experiment name
+TASK_TYPE="rec"
+cd ${REPO_HOME}/src/open-r1-multimodal
+
+export DEBUG_MODE="true" # Enable Debug if you want to see the rollout of model during RL
+# create the run directory and log file
+mkdir -p ${REPO_HOME}/runs/${EXP_NAME}/log
+export LOG_PATH="${REPO_HOME}/runs/${EXP_NAME}/log/debug_log.$(date +%Y-%m-%d-%H-%M-%S).txt"
+# MAX_STEPS=1200 # TODO: change this to your own max steps
+
+
+# export WANDB_DISABLED=true
+# CUDA_VISIBLE_DEVICES=4,5,6,7
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node="8" \
+    --nnodes="1" \
+    --node_rank="0" \
+    --master_addr="127.0.0.1" \
+    --master_port="12349" \
+  src/open_r1/grpo_jsonl.py \
+    --use_vllm False \
+    --output_dir /dev/shm/internvl3_8B_lsvq_grpo \
+    --resume_from_checkpoint True \
+    --model_name_or_path $model_path \
+    --data_file_paths $data_paths \
+    --image_folders $image_folders \
+    --is_reward_customized_from_vlm_module $is_reward_customized_from_vlm_module \
+    --task_type $TASK_TYPE \
+    --max_anyres_num 6 \
+    --per_device_train_batch_size 8 \
+    --gradient_accumulation_steps 2 \
+    --gradient_checkpointing true \
+    --logging_steps 1 \
+    --num_train_epochs 3 \
+    --bf16 \
+    --attn_implementation flash_attention_2 \
+    --run_name ${EXP_NAME} \
+    --data_seed 42 \
+    --save_steps 50 \
+    --save_total_limit 1 \
+    --num_generations 4 \
+    --max_completion_length 2048 \
+    --reward_funcs accuracy format diversity \
+    --beta 0.04 \
+    --report_to none \
+    --dataset-name this_is_not_used \
+    --deepspeed ${REPO_HOME}/src/open-r1-multimodal/local_scripts/zero2.json \
+
+echo "Training completed for ${EXP_NAME}"
